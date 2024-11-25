cfdcc6ae286d8248d22ed536b8ea894a3e57f89b
andrama <andrama@stud.ntnu.no>
2024-11-26 00:03:30 +0100

    created the python module

--- notebook/utils/__init__.py
+++ notebook/utils/__init__.py
@@ -1,2 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from .data_aggregator import DataAggregator
+from .data_fetcher import DataFetcher
+from .data_visualizator import *
+
+from .models.ELO_model import ELOModel
+
+__all__ = ["DataAggregator", "DataFetcher", "plot_column_distributions", "ELOModel"]
